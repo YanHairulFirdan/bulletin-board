f7173e8dad618d8659d794c2fbe358c6f2c4c612
yanhf <yanhairulfirdan@gmail.com>
2021-05-04 10:31:21 +0700

    change debug file name to functions

--- save.php
+++ save.php
@@ -1,6 +1,6 @@
 <?php
 // require_once('config/database_connection.php');
-require_once('helpers/debug.php');
+require_once('helpers/functions.php');
 require_once('utils/Validation.php');
 require_once('Database/Model.php');
 
