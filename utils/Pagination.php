03e8958725b63490bb704f8c4096d2e50d7c3d3b
yanhf <yanhairulfirdan@gmail.com>
2021-05-10 13:02:37 +0700

    fixing pagination

--- utils/Pagination.php
+++ utils/Pagination.php
@@ -12,30 +12,44 @@ class Pagination
         $numberOfRecords               = $this->model->numberOfRecord();
         $numberOfPager                 = floor($numberOfRecords / $this->model->dataPerPage);
         $numberOfPager                += ($numberOfRecords % $this->model->dataPerPage > 0) ? 1 : 0;
+
         $this->currentPage             = (isset($_REQUEST['page'])) ? intval($_REQUEST['page']) : 1;
         $this->nextPage                = ($this->currentPage != $numberOfPager) ? $this->currentPage + 1 : 0;
         $this->previousPage            = ($this->currentPage > 1) ? $this->currentPage -  1 : 0;
         $this->pager                   = 5;
 
-
+        echo $this->currentPage;
 
         if ($numberOfPager <= 5) {
             $this->startIndex       = 1;
             $this->pager            = $numberOfPager;
         } else if ($numberOfPager > 5) {
             $this->startIndex       = ($this->currentPage - 2 >= 2) ? $this->currentPage - 2 : 1;
-
             if ($numberOfPager - $this->currentPage <= 2) {
                 $pagerButton        = $numberOfPager;
-
                 $this->startIndex   = ($numberOfPager - $this->currentPage == 0) ? $this->currentPage - 4 : $this->currentPage - 3;
             } else {
-                $pagerButton        = $this->currentPage + 2;
+
+                $pagerButton        = ($this->currentPage <= 2) ? 5 : $this->currentPage + 2;
             }
         }
 
+        // echo "<pre>";
+        // print_r([
+        //     $this->currentPage,
+        //     $this->startIndex,
+        //     $this->previousPage,
+        //     $this->nextPage,
+        //     $pagerButton
+        // ]);
+        // echo "</pre>";
+        // die;
         return [
-            $this->currentPage, $this->startIndex, $this->previousPage, $this->nextPage, $pagerButton
+            $this->currentPage,
+            $this->startIndex,
+            $this->previousPage,
+            $this->nextPage,
+            $pagerButton
         ];
     }
 }
