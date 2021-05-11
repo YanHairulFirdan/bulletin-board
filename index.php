e911aa06c32f3940490ae05ccf5436f8e1328b3b
yanhf <yanhairulfirdan@gmail.com>
2021-05-11 16:42:02 +0700

    coding standar for index.php page

--- index.php
+++ index.php
@@ -7,11 +7,12 @@ require_once('utils/Validation.php');
 require_once('utils/Pagination.php');
 
 
-$databaseConnection          = DatabaseConnection::getInstance();
-$bulletinsModel              = new Model($databaseConnection, 'bulletins');
-$pagination                  = new Pagination($bulletinsModel);
-$bulletins                   = $bulletinsModel->getData();
+$databaseConnection = DatabaseConnection::getInstance();
+$bulletinsModel     = new Model($databaseConnection, 'bulletins');
+$pagination         = new Pagination($bulletinsModel);
+$bulletins          = $bulletinsModel->getData();
 
+// change to pagination getCurrentPage() for example
 list(
     $currentPage,
     $startIndex,
@@ -19,18 +20,18 @@ list(
     $nextPage,
     $pagerButton
 )                           = $pagination->paginator();
-
+// change for pagination
 
 if ($_POST) {
     $rules = [
-        'title'     => 'required|length:10-32',
-        'body'      => 'required|length:10-220',
+        'title' => 'required|length:10-32',
+        'body'  => 'required|length:10-220',
     ];
 
-    $validation     = new Validation($rules);
-    $errorMessages  = $validation->validate($_POST);
-    $body           = $_POST['body'];
-    $title          = $_POST['title'];
+    $validation    = new Validation($rules);
+    $errorMessages = $validation->validate($_POST);
+    $body          = $_POST['body'];
+    $title         = $_POST['title'];
     if (!$errorMessages) {
         $bulletinsModel->create($_POST);
     }
