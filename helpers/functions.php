d48c46b245ff2924f6481194cb6926aeb01f8d33
yanhf <yanhairulfirdan@gmail.com>
2021-05-18 13:46:20 +0700

    delete unnecessary function in functions.php

--- helpers/functions.php
+++ helpers/functions.php
@@ -5,20 +5,3 @@ function dump($data)
     print_r($data);
     echo "</pre>";
 }
-
-function replace_params()
-{
-    $errorMessage = [];
-    foreach ($_GET as $key => $get) {
-        // echo $key;
-        echo stristr($key, 'error') ? 'true' : 'false';
-        if (stristr($key, 'error') || stristr($key, 'success')) {
-            echo $get;
-            // $get = str_replace('+', ' ', $get);
-            // $p
-            $errorMessage[$key] = str_replace('+', ' ', $get);
-        }
-    }
-
-    return (count($errorMessage) > 0) ? $errorMessage : null;
-}
