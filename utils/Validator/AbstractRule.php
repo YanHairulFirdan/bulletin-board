5aad5502f217258e4fd47ff01ca0a1df4dde310c
yanhf <yanhairulfirdan@gmail.com>
2021-05-21 09:31:04 +0700

    implement coding standard in abstract rule class

--- utils/Validator/AbstractRule.php
+++ utils/Validator/AbstractRule.php
@@ -5,7 +5,9 @@ namespace App\Utils\Validator;
 abstract class AbstractRule
 {
     protected $message;
+
     public abstract function check($data);
+
     public function getMessage()
     {
         return $this->message;
