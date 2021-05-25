7732bfa8c84db54aa9d2a7081045311545e9987f
yanhf <yanhairulfirdan@gmail.com>
2021-05-25 08:26:41 +0700

    fixing unit in size validator class

--- utils/Validator/Size.php
+++ utils/Validator/Size.php
@@ -5,11 +5,12 @@ namespace App\Utils\Validator;
 
 class Size extends AbstractRule
 {
+    private $unit = 1000;
     public function check($data)
     {
         $field   = $data['field'];
-        $maxSize = $data['requisite'] / 1000;
-        $size    = $_FILES[$data['field']]['size'] / 1000;
+        $maxSize = $data['requisite'];
+        $size    = $_FILES[$data['field']]['size']  / $this->unit;
 
         if ($size > $maxSize) {
             $this->message = "File size for {$field} is {$maxSize} MB";
