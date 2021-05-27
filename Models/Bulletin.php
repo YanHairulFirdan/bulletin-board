f928608e56eb7254c097e7074fae4e1db2ea3764
yanhf <yanhairulfirdan@gmail.com>
2021-05-28 05:21:49 +0700

    move model parent class to Lib\Model Folder

--- Models/Bulletin.php
+++ Models/Bulletin.php
@@ -2,7 +2,7 @@
 
 namespace App\Model;
 
-use App\Model\Model;
+use Lib\Model\Model as Model;
 
 class Bulletin extends Model
 {
--- lib/Model/Model.php
+++ lib/Model/Model.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Model;
+namespace Lib\Model;
 
 use Lib\Database\Database;
 
