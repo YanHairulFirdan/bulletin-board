10d7d0581b52520ef5a09f56ae9a30146a149aa5
yanhf <yanhairulfirdan@gmail.com>
2021-05-28 05:23:51 +0700

    move models to App\Model, config to App\Config, Utils to Lib\Utils

---
+++ App/config/config.php
@@ -0,0 +1,10 @@
+<?php
+define("DATABASE_TYPE", "mysql");
+define("HOST", "localhost");
+define("DATABASE_NAME", "bulletinboard");
+define("USERNAME", "root");
+define("PASSWORD", "");
+
+
+// if you are using sqlite, please assign value for this constant
+define("DATABASE_DIRECTORY", ":/");
