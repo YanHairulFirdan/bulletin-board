63bc6cb5b9895921761ac38eff30563e48254db2
yanhf <yanhairulfirdan@gmail.com>
2021-06-09 14:04:04 +0700

    deleteing load_view and root_path function in functions.php

--- helpers/functions.php
+++ helpers/functions.php
@@ -7,21 +7,6 @@ function dump($data)
     echo "</pre>";
 }
 
-function load_view(string $viewName, array $viewVariables = [])
-{
-    if (!file_exists("public/assets/views/{$viewName}.view.php")) {
-        throw new Exception("file not exists", 1);
-    }
-
-    require_once "public/assets/views/{$viewName}.view.php";
-}
-
-
-function root_path()
-{
-    return str_replace('\\', '/', dirname(__DIR__));
-}
-
 function redirect($url, $permanent = true)
 {
     header("Location: {$url}", true, $permanent ? 301 : 302);
