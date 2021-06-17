e83938c55684ebc6b50670f6fcd99f85f0a8d267
yanhf <yanhairulfirdan@gmail.com>
2021-06-17 15:29:32 +0700

    delete unnecessary constant

--- config/app.php
+++ config/app.php
@@ -1,12 +1,11 @@
 <?php
 define("DEBUG", 1);
 define("TIME_ZONE", "Asia/Makassar");
-define("ERROR_LEVEL", E_ALL);
 define("ROOT", dirname(__DIR__));
 
-error_reporting(ERROR_LEVEL);
+error_reporting(E_ALL);
 
 ini_set('date.timezone', TIME_ZONE);
 ini_set('display_errors', DEBUG);
 ini_set('display_startup_errors', DEBUG);
-ini_set('log_errors', 1);
+ini_set('error_log', ROOT . '\logs\\error.log');
