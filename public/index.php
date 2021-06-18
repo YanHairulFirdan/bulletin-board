5bcbd1a06d72ae18ef96df7a444080749e6274cb
YanHF <yan.1754@students.amikom.ac.id>
2021-06-18 10:55:42 +0700

    change class name from errorlog to errorLogger

--- public/index.php
+++ public/index.php
@@ -4,12 +4,12 @@ require_once('../vendor/autoload.php');
 
 use App\Models\Bulletin;
 use App\Validations\BulletinValidation;
-use Lib\Logger\ErrorLog;
+use Lib\Logger\ErrorLogger;
 use Lib\Pagination\Pagination;
 
 try {
     $bulletin   = new Bulletin();
-    $pagination = new Pagination(30, 10, 2);
+    $pagination = new Pagination(15, 10, 2);
 
     $page  = (isset($_GET['page']) && !empty($_GET['page'])) ? $_GET['page'] : 1;
     $limit = $pagination->dataPerPage;
@@ -33,7 +33,7 @@ try {
 
     require_once "assets/views/index.view.php";
 } catch (\Throwable $e) {
-    ErrorLog::logMessage($e->getMessage());
+    ErrorLogger::logMessage($e->getMessage());
 
     redirect('error.php');
 }
