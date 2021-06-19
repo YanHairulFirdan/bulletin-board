8dbd46e3318c997f03e6b93216ea45108317a313
YanHF <yan.1754@students.amikom.ac.id>
2021-06-20 05:37:06 +0700

    update app.php file

--- config/app.php
+++ config/app.php
@@ -1,12 +1,29 @@
 <?php
 define("DEBUG", 1);
-define("MODE", "Development");
+define("MODE", "development");
 define("TIME_ZONE", "Asia/Makassar");
 define("ROOT", dirname(__DIR__));
 
-error_reporting(E_ALL);
 
-ini_set('date.timezone', TIME_ZONE);
-ini_set('display_errors', DEBUG);
-ini_set('display_startup_errors', DEBUG);
-ini_set('error_log', ROOT . '\logs\\error.log');
+// ini_set('date.timezone', TIME_ZONE);
+// ini_set('display_errors', DEBUG);
+// ini_set('display_startup_errors', DEBUG);
+// ini_set('error_log', ROOT . '\logs\\error.log');
+switch (MODE) {
+    case 'development':
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+        break;
+    case 'production':
+        ini_set('date.timezone', TIME_ZONE);
+        ini_set('display_errors', 0);
+        ini_set('display_startup_errors', 0);
+        ini_set('error_log', ROOT . '\logs\\error.log');
+        error_reporting(E_ALL);
+        break;
+    
+    default:
+        error_reporting(E_ALL);
+        break;
+}
