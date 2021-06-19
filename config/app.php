f1ec4ee2f9f88b2c7c4e1673a9879bf21ab7502d
YanHF <yan.1754@students.amikom.ac.id>
2021-06-20 05:41:22 +0700

    move codes from init.php to app.php

--- config/app.php
+++ config/app.php
@@ -1,6 +1,6 @@
 <?php
-define("DEBUG", 1);
-define("MODE", "development");
+// define("DEBUG", 1);
+define("MODE", "production");
 define("TIME_ZONE", "Asia/Makassar");
 define("ROOT", dirname(__DIR__));
 
@@ -20,7 +20,7 @@ switch (MODE) {
         ini_set('display_errors', 0);
         ini_set('display_startup_errors', 0);
         ini_set('error_log', ROOT . '\logs\\error.log');
-        error_reporting(E_ALL);
+        error_reporting(0);
         break;
     
     default:
