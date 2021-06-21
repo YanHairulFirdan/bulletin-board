c27ec8709cca4d315cc1ef88befffa4f7f68bc8a
YanHF <yan.1754@students.amikom.ac.id>
2021-06-21 15:36:25 +0700

    add mode staging in mode type

--- config/app.php
+++ config/app.php
@@ -1,26 +1,25 @@
 <?php
-define("MODE", "production");
+define("MODE", "development");
 define("TIME_ZONE", "Asia/Makassar");
 define("ROOT", dirname(__DIR__));
 
 set_exception_handler(['Lib\Handler\ExceptionHandler', 'handle']);
-error_reporting(E_ALL);
 ini_set('date.timezone', TIME_ZONE);
-    
+
 switch (MODE) {
     case 'development':
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
-        // error_reporting(E_ALL);
+        error_reporting(E_ALL);
         break;
-
+    case 'staging':
     case 'production':
         ini_set('display_errors', 0);
         ini_set('display_startup_errors', 0);
         ini_set('error_log', ROOT . '\logs\\error.log');
         error_reporting(0);
         break;
-    
+
     default:
         break;
 }
--- lib/handler/ExceptionHandler.php
+++ lib/handler/ExceptionHandler.php
@@ -4,18 +4,18 @@ namespace Lib\Handler;
 
 use Lib\Logger\ErrorLogger;
 
-class ExceptionHandler  
+class ExceptionHandler
 {
     public static function handle(\Throwable $th)
     {
-        $message = http_response_code().' ' . $th->getCode() . ' : '.$th->getMessage().' in line '.$th->getLine().' inside file '. $th->getFile();
+        $message = $th->getCode() . ' : ' . $th->getMessage() . ' in line ' . $th->getLine() . ' inside file ' . $th->getFile();
 
-        if (MODE == 'production') {
-            ErrorLogger::logMessage($message);
-            
-            redirect('error.php');
-        }else {
+        if (MODE == 'development') {
             dump($message);
+        } else {
+            ErrorLogger::logMessage($message);
+
+            redirect('error.php');
         }
     }
 }
